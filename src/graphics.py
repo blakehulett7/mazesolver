02941fcea4a8946a38ee6bfb21caee7a55b44e6b
blakehulett7 <blake.hulett7@gmail.com>
2024-06-04 12:56:53 -0500

    Made all attributes private to avoid future conflicts!

--- src/graphics.py
+++ src/graphics.py
@@ -3,23 +3,23 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.root = Tk()
-        self.root.title = "FireMage Maze Solver"
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-        self.canvas = Canvas(self.root, bg="white",
-                             width=width, height=height)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.running = False
+        self.__root = Tk()
+        self.__root.title = "FireMage Maze Solver"
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.root, bg="white",
+                               width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
 
     def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
+        self.__root.update_idletasks()
+        self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running is True:
+        self.__running = True
+        while self.__running is True:
             self.redraw()
         print("Window Closed")
 
     def close(self):
-        self.running = False
+        self.__running = False
