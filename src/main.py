7d82f21ffaf2d3f8a28b478e2c373109285b3640
blakehulett7 <blake.hulett7@gmail.com>
2024-06-06 13:45:09 -0500

    Finishing touches LFG!!

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ from logic import Cell, Maze
 def main():
     win = Window(800, 600)
 
-    test_maze = Maze(50, 50, 10, 14, 50, win, 7)
-    test_maze.animate("black")
-    test_maze.break_entrance_and_exit()
-    test_maze.break_interior_walls(test_maze.entrance)
-    test_maze.reset_visited()
-    test_maze.solver(test_maze.entrance)
+    goated_maze = Maze(50, 50, 10, 14, 50, win)
+    goated_maze.animate("black")
+    goated_maze.break_entrance_and_exit()
+    goated_maze.break_interior_walls(goated_maze.entrance)
+    goated_maze.reset_visited()
+    goated_maze.solver(goated_maze.entrance)
 
     win.wait_for_close()
 
